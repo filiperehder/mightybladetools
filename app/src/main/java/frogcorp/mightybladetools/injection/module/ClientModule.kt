d6a08aa25549e6b5d5fe895e9f8d68cf5bf84316
Filipe Rehder <filipe.rehder@gmail.com>
2018-03-18 15:15:13 -0300

    add ClientModule

---
+++ app/src/main/java/frogcorp/mightybladetools/injection/module/ClientModule.kt
@@ -0,0 +1,53 @@
+package frogcorp.mightybladetools.injection.module
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import frogcorp.mightybladetools.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class ClientModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(gson: Gson, okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .baseUrl("https://mighty-blade-tools.firebaseio.com/")
+                .client(okHttpClient)
+                .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        val level = getInterceptorLevel()
+        httpLoggingInterceptor.level = level
+
+        return OkHttpClient.Builder()
+                .addInterceptor(httpLoggingInterceptor)
+                .build()
+    }
+
+    private fun getInterceptorLevel(): HttpLoggingInterceptor.Level {
+        return if (BuildConfig.DEBUG)
+            HttpLoggingInterceptor.Level.BODY
+        else
+            HttpLoggingInterceptor.Level.NONE
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder().create()
+    }
+}
